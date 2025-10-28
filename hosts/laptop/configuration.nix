2159f922b4a28ef5a770e2e52b2102be8d0eb9bc
Thomas Le Magoariec <thomas.lemagoariec@outlook.com>
2025-10-28 20:15:35 +0100

    structure

---
+++ hosts/laptop/configuration.nix
@@ -0,0 +1,47 @@
+{ inputs, config, pkgs, ... }:
+
+{
+
+    imports =
+    [
+        ./hardware-configuration.nix
+        ../../modules/default.nix
+    ];
+
+
+    # Bootloader.
+    #boot.loader.systemd-boot.enable = true;
+    #boot.loader.efi.canTouchEfiVariables = true;
+
+    # shoud be set in the host/default.nix
+    #networking.hostName = "nixos"; # Define your hostname.
+    networking.networkmanager.enable = true;
+
+    hardware.bluetooth.enable = true; # enables support for Bluetooth
+    hardware.bluetooth.powerOnBoot = true;
+
+    security.rtkit.enable = true; # no clue what this is so won't touch it
+
+
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    environment.systemPackages = with pkgs; [
+        home-manager
+        pkg-config
+        openssl
+        autoconf
+        automake
+        libtool
+        gettext
+    ];
+
+    # Enable the OpenSSH daemon.
+    services.openssh.enable = true;
+
+    system.stateVersion = "25.05"; # Did you read the comment?
+
+}
---
+++ modules/default.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, host, ... }:
+
+{
+    environment.systemPackages = with pkgs; [
+        neovim
+        git
+        tmux
+        kitty
+        python3                 # python 3.12.9
+        python3Packages.pip     # pip (the worlds worst package manager)
+        nodejs                  # useful for anything and everything
+        gcc11                   # C/C++
+
+        pyright                 # python LSP
+        nix-ld
+    ];
+
+    programs.zsh.enable = true;
+
+}
+
