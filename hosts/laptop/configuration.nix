e432a134581efd5da8b2aa25bd70a6833af60e32
Thomas <thomas.lemagoariec@gmail.com>
2025-10-29 20:25:19 +0000

    rm gdm

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -42,8 +42,7 @@
 
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
 
   # Configure keymap in X11
