eb2289178bde0b37f3760aeff3d50cb78e423e85
Thomas <thomas.lemagoariec@gmail.com>
2025-10-29 19:42:48 +0000

    hyprland config

--- hm-modules/hypr.nix
+++ hm-modules/hypr.nix
@@ -5,8 +5,8 @@
       "$mod" = "SUPER";
       bind =
           [
-          "$mod, F, exec, kitty"
-              ", Print, exec, grimblast copy area"
+            "$mod, F, exec, kitty"
+            ", Print, exec, grimblast copy area"
           ]
           ++ (
                 builtins.concatLists (builtins.genList (i:
@@ -19,6 +19,24 @@
                 9)
             );
   };
+
+  wayland.windowManager.hyprland.extraConfig = ''
+        input {
+    kb_layout = fr
+    kb_variant =
+    kb_model =
+    kb_options =
+    kb_rules =
+
+    follow_mouse = 1
+
+    sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+
+    touchpad {
+        natural_scroll = true
+    }
+}
+  '';
   # Optional, hint Electron apps to use Wayland:
   # home.sessionVariables.NIXOS_OZONE_WL = "1";
 }
