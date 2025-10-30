9df1af6a914d71b93c75505ccdb16c3adce25a0d
Thomas <thomas.lemagoariec@gmail.com>
2025-10-30 14:03:46 +0000

    fix: hyprland move to workspace

--- hm-modules/hypr.nix
+++ hm-modules/hypr.nix
@@ -21,17 +21,16 @@
             "$mod, underscore, workspace, 8"
             "$mod, ccedilla, workspace, 9"
             "$mod, agrave, workspace, 10"
-
-            "$mod, SHIFT, ampersand, movetoworkspace, 1"
-            "$mod, SHIFT, eacute, movetoworkspace, 2"
-            "$mod, SHIFT, quotedbl, movetoworkspace, 3"
-            "$mod, SHIFT, apostrophe, movetoworkspace, 4"
-            "$mod, SHIFT, parenleft, movetoworkspace, 5"
-            "$mod, SHIFT, minus, movetoworkspace, 6"
-            "$mod, SHIFT, egrave, movetoworkspace, 7"
-            "$mod, SHIFT, underscore, movetoworkspace, 8"
-            "$mod, SHIFT, ccedilla, movetoworkspace, 9"
-            "$mod, SHIFT, agrave, movetoworkspace, 10"
+            "$mod_SHIFT, ampersand, movetoworkspace, 1"
+            "$mod_SHIFT, eacute, movetoworkspace, 2"
+            "$mod_SHIFT, quotedbl, movetoworkspace, 3"
+            "$mod_SHIFT, apostrophe, movetoworkspace, 4"
+            "$mod_SHIFT, parenleft, movetoworkspace, 5"
+            "$mod_SHIFT, minus, movetoworkspace, 6"
+            "$mod_SHIFT, egrave, movetoworkspace, 7"
+            "$mod_SHIFT, underscore, movetoworkspace, 8"
+            "$mod_SHIFT, ccedilla, movetoworkspace, 9"
+            "$mod_SHIFT, agrave, movetoworkspace, 10"
           ];
   };
 
