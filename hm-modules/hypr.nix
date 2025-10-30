8687d0e2ca0d033f99389b7d5bd25e219f196e10
Thomas <thomas.lemagoariec@gmail.com>
2025-10-30 13:53:38 +0000

    hyprland move to workspace

--- hm-modules/hypr.nix
+++ hm-modules/hypr.nix
@@ -21,6 +21,17 @@
             "$mod, underscore, workspace, 8"
             "$mod, ccedilla, workspace, 9"
             "$mod, agrave, workspace, 10"
+
+            "$mod, SHIFT, ampersand, movetoworkspace, 1"
+            "$mod, SHIFT, eacute, movetoworkspace, 2"
+            "$mod, SHIFT, quotedbl, movetoworkspace, 3"
+            "$mod, SHIFT, apostrophe, movetoworkspace, 4"
+            "$mod, SHIFT, parenleft, movetoworkspace, 5"
+            "$mod, SHIFT, minus, movetoworkspace, 6"
+            "$mod, SHIFT, egrave, movetoworkspace, 7"
+            "$mod, SHIFT, underscore, movetoworkspace, 8"
+            "$mod, SHIFT, ccedilla, movetoworkspace, 9"
+            "$mod, SHIFT, agrave, movetoworkspace, 10"
           ];
   };
 
