5ae776fb9ce3e1f75a39dfc8e53c2ea1629adec6
Thomas <thomas.lemagoariec@gmail.com>
2025-10-30 19:16:42 +0100

    dotfiles link

--- hm-modules/dotfiles.nix
+++ hm-modules/dotfiles.nix
@@ -5,10 +5,10 @@
     home.file = {
         ".p10k.zsh".source = ../dotfiles/p10k.zsh;
 
-        #".config/waybar/power_menu.xml".source = ../dotfiles/waybar/power_menu.xml;
-        #".config/waybar/network_menu.xml".source = ../dotfiles/waybar/network_menu.xml;
-        #".config/waybar/power_menu.xml".source = ../dotfiles/waybar/power_menu.xml;
-        #".config/waybar/power_menu.xml".source = ../dotfiles/waybar/power_menu.xml;
-        #".config/waybar/power_menu.xml".source = ../dotfiles/waybar/power_menu.xml;
+        ".config/waybar/power_menu.xml".source = ../dotfiles/waybar/power_menu.xml;
+        ".config/waybar/network_menu.xml".source = ../dotfiles/waybar/network_menu.xml;
+        ".config/waybar/config.jsonc".source = ../dotfiles/waybar/config.jsonc;
+        ".config/waybar/style.css".source = ../dotfiles/waybar/style.css;
+        ".config/hypr/hypridle.conf".source = ../dotfiles/hypr/hypridle.conf;
     };
 }
