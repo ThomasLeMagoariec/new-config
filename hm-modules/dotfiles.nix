365d68737d3c3005578b9294463c945f20d659af
Thomas <thomas.lemagoariec@gmail.com>
2025-10-31 14:35:41 +0100

    rm hypridle.conf

--- hm-modules/dotfiles.nix
+++ hm-modules/dotfiles.nix
@@ -10,7 +10,6 @@
         ".config/waybar/config.jsonc".source = ../dotfiles/waybar/config.jsonc;
         ".config/waybar/style.css".source = ../dotfiles/waybar/style.css;
 
-        #".config/hypr/hypridle.conf".source = ../dotfiles/hypr/hypridle.conf;
         ".config/hypr/hyprpaper.conf".source = ../dotfiles/hypr/hyprpaper.conf;
         ".config/hypr/wallpaper.jpg".source = ../dotfiles/static/wallpaper.jpg;
     };
