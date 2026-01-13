1094bfe03ec883ce2fb81bff7aa1119a0276f1f1
Thomas <thomas.lemagoariec@outlook.com>
2026-01-13 23:29:00 +0100

    secrets

--- nix-modules/sops.nix
+++ nix-modules/sops.nix
@@ -24,6 +24,9 @@
             "thomas/private_keys/gmaps_api" = {
                 owner = config.users.users.thomas.name;
             };
+            "thomas/private_keys/mistral_api" = {
+                owner = config.users.users.thomas.name;
+            };
             "thomas/user/password" = {
                 owner = config.users.users.thomas.name;
             };
