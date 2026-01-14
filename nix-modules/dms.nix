58e04f4ae0e154b4ebfdd9858d81c5505b85fb34
Thomas <thomas.lemagoariec@outlook.com>
2026-01-14 16:23:16 +0100

    dms

--- nix-modules/dms.nix
+++ nix-modules/dms.nix
@@ -1,23 +1,8 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs-unstable, ... }:
 {
-    environment.systemPackages = with pkgs; [
-        dms
+    environment.systemPackages = with pkgs-unstable; [
+        quickshell
+        dms-shell
     ];
 
-    programs.dms = {
-        enable = true;
-
-        systemd = {
-            enable = true;             # Systemd service for auto-start
-                restartIfChanged = true;   # Auto-restart dms.service when dms-shell changes
-        };
-
-# Core features
-        enableSystemMonitoring = true;     # System monitoring widgets (dgop)
-            enableClipboard = true;            # Clipboard history manager
-            enableVPN = true;                  # VPN management widget
-            enableDynamicTheming = true;       # Wallpaper-based theming (matugen)
-            enableAudioWavelength = true;      # Audio visualizer (cava)
-            enableCalendarEvents = true;       # Calendar integration (khal)
-    };
 }
