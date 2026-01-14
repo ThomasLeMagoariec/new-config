00c81bb102b12f20a4f435e872a6ede9b205d6cd
Thomas <thomas.lemagoariec@outlook.com>
2026-01-14 18:07:29 +0100

    dms

--- hm-modules/dms.nix
+++ hm-modules/dms.nix
@@ -7,6 +7,18 @@
     programs.dank-material-shell = {
         enable = true;
         dgop.package = pkgs-unstable.dgop;
+
+        systemd = {
+            enable = true;             # Systemd service for auto-start
+            restartIfChanged = true;   # Auto-restart dms.service when dms-shell changes
+        };
+
+# Core features
+        enableSystemMonitoring = true;     # System monitoring widgets (dgop)
+        enableVPN = true;                  # VPN management widget
+        enableDynamicTheming = true;       # Wallpaper-based theming (matugen)
+        enableAudioWavelength = true;      # Audio visualizer (cava)
+        enableCalendarEvents = true;
     };
 }
 
