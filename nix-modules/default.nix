cf7d49f03d80a25bab782732db39fda970f98dc0
Thomas <thomas.lemagoariec@outlook.com>
2026-01-16 21:46:45 +0100

    rm fprint

--- nix-modules/default.nix
+++ nix-modules/default.nix
@@ -5,7 +5,6 @@
         ./kb.nix
         ./bluetooth.nix
         ./sops.nix
-        ./fprint.nix
 	] ++ lib.optionals (preferences.openssh == true) [ ./openssh.nix ];
     
 
--- nix-modules/fprint.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }: {
-    environment.systemPackages = with pkgs; [
-        fprintd
-    ];
-
-    services.fprintd.enable = true;
-}
