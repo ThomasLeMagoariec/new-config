0705f38172db6a65f7e02eb16e65956a0ce6f22c
Thomas <thomas.lemagoariec@outlook.com>
2026-01-16 01:46:50 +0100

    clean nixvim

--- hm-modules/nixvim/keybindings.nix
+++ hm-modules/nixvim/keybindings.nix
@@ -1,4 +1,5 @@
-{self, ...}: {
+{ ... }: 
+{
     programs.nixvim.keymaps = [
         { key = "<leader>ca"; action = "<cmd>lua vim.lsp.buf.code_action()<CR>"; }
         { key = "<leader>fe"; action = "<cmd>Neotree filesystem reveal left<CR>"; }
