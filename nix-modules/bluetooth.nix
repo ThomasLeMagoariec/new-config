5478b361d74f4391fc04384372403dbf45fa3b28
Thomas <thomas.lemagoariec@outlook.com>
2026-01-19 18:17:57 +0100

    bluetooth vendorID set to apple

--- nix-modules/bluetooth.nix
+++ nix-modules/bluetooth.nix
@@ -1,5 +1,14 @@
 { config, ... }:
 {
-    hardware.bluetooth.enable = true;
     services.blueman.enable = true;
+
+    hardware.bluetooth = {
+        enable = true;
+
+        settings = {
+            General = {
+                DeviceID = "bluetooth:004C:0000:0000";
+            };
+        };
+    };
 }
