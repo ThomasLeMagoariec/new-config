e8b4a71def80f95cc2df72c9dfbf134dd360ac90
Thomas <thomas.lemagoariec@outlook.com>
2026-01-28 13:42:38 +0100

    yubi

--- nix-modules/default.nix
+++ nix-modules/default.nix
@@ -5,6 +5,7 @@
         ./kb.nix
         ./bluetooth.nix
         ./sops.nix
+        ./yubi.nix
 	] ++ lib.optionals (preferences.openssh == true) [ ./openssh.nix ];
     
 
---
+++ nix-modules/yubi.nix
@@ -0,0 +1,32 @@
+{ pkgs, lib, ... }:
+{
+    environment.systemPackages = with pkgs; [
+        yubioath-flutter
+        yubikey-manager
+        pam_u2f
+    ];
+
+    services.pcscd.enable = true;
+    services.udev.packages = [ pkgs.yubikey-personalization ];
+
+    services.yubikey-agent.enable = true;
+
+    security.pam = {
+        sshAgentAuth.enable = true;
+        u2f = {
+            enable = true;
+            settings = {
+                cue = false;
+                authFile = "/home/thomas/.config/Yubico/u2f_keys";
+            };
+        };
+        
+        services = {
+            login.u2fAuth = true;
+            sudo = {
+                u2fAuth = true;
+                sshAgentAuth = true;
+            };
+        };
+    };
+}
