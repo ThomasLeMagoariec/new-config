e8460a7db5b3b8138733c8b8f76d32adb3e55dda
Thomas <thomas.lemagoariec@outlook.com>
2026-02-11 18:05:42 +0100

    testing

--- hm-modules/test.nix
+++ hm-modules/test.nix
@@ -1,13 +1,11 @@
 {lib, pkgs, ...}:
 let
-    prefs =
-        if builtins.pathExists ../prefs.nix 
-        then import ../prefs.nix
-        else {}
+    hasPrefs = builtins.pathExists ../prefs.nix;
+    prefs = if hasPrefs then import ../prefs.nix else {};
 in
     pkgs.writeShellApplication {
         name = "bite";
         text = ''
-            echo value from prefs: ${prefs.bite}
+            echo value from prefs: 
         '';
     }
--- prefs.nix
+++ prefs.nix
@@ -1,3 +1,3 @@
 {
-    bite = "zeub"
+    bite = "zeub";
 }
