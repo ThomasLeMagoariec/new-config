c1916af6c18a6f16770eae52c1cb9684cc0d720c
thomas <thomas.lemagoariec@outlook.com>
2026-05-20 18:57:00 +0200

    direnv

--- hm-modules/default.nix
+++ hm-modules/default.nix
@@ -11,6 +11,7 @@ in
         ./sops.nix
         ./ssh.nix
         ./nixmate.nix
+        ./direnv.nix
         ./tmux.nix # will move into an option eventually
 	] ++ lib.optionals (prefs.shell == "zsh") [ ./zsh.nix ]
       ++ lib.optionals (prefs.dms == true) [ ./dms.nix ./dsearch.nix ]
