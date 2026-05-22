f6ac9b2ff63d61172167d1a0e6b0d0e435f91d03
thomas <thomas.lemagoariec@outlook.com>
2026-05-22 18:43:07 +0200

    opt niri

--- hm-modules/pkgs/opt-niri.nix
+++ hm-modules/pkgs/opt-niri.nix
@@ -174,6 +174,168 @@ in
             default = {};
             description = "input section of the niri config";
         };
+
+        layout = mkOption {
+            type = types.submodule {
+                options = {
+                    gaps = mkOption {
+                        type = types.number;
+                        default = 8;
+                    };
+                    center-focused-column = mkOption {
+                        type = types.str;
+                        default = "never";
+                    };
+                    preset-column-widths = mkOption {
+                        type = types.submodule {
+                            options = {
+                                proportions = mkOption {
+                                    type = types.listOf types.float;
+                                    default = [ 0.33333 0.5 0.66667 ];
+                                };
+                            };
+                        };
+                        default = {};
+                        description = "preset column widths";
+                    };
+                    default-column-width = mkOption {
+                        type = types.submodule {
+                            options = {
+                                proportion = mkOption {
+                                    type = types.float;
+                                    default = 0.5;
+                                };
+                            };
+                        };
+                        default = {};
+                        description = "default column width config";
+
+                    };
+                    focus-ring = mkOption {
+                        type = types.submodule {
+                            options = {
+                                off = mkOption {
+                                    type = types.bool;
+                                    default = false;
+                                };
+                                width = mkOption {
+                                    type = types.number;
+                                    default = 4;
+                                };
+                                active-color = mkOption {
+                                    type = types.str;
+                                    default = "#7fc8ff";
+                                };
+                                inactive-color = mkOption {
+                                    type = types.str;
+                                    default = "#505050";
+                                };
+                            };
+                        };
+                        default = {};
+                        description = "focus ring config";
+                    };
+                    border = mkOption {
+                        type = types.submodule {
+                            options = {
+                                off = mkOption {
+                                    type = types.bool;
+                                    default = true;
+                                };
+                                width = mkOption {
+                                    type = types.number;
+                                    default = 4;
+                                };
+                                active-color = mkOption {
+                                    type = types.str;
+                                    default = "#ffc87f";
+                                };
+                                inactive-color = mkOption {
+                                    type = types.str;
+                                    default = "#505050";
+                                };
+                                urgent-color = mkOption {
+                                    type = types.str;
+                                    default = "#9b0000";
+                                };
+                            };
+                        };
+                        default = {};
+                        description = "border config";
+                    };
+                    shadow = mkOption {
+                        type = types.submodule {
+                            options = {
+                                on = mkOption {
+                                    type = types.bool;
+                                    default = false;
+                                };
+                                draw-behind-window = mkOption {
+                                    type = types.bool;
+                                    default = false;
+                                };
+                                softness = mkOption {
+                                    type = types.number;
+                                    default = 30;
+                                };
+                                spread = mkOption {
+                                    type = types.number;
+                                    default = 5;
+                                };
+                                offset = mkOption {
+                                    type = types.str;
+                                    default = "x=0 y=5";
+                                };
+                                color = mkOption {
+                                    type = types.str;
+                                    default = "#0007";
+                                };
+                            };
+                        };
+                        default = {};
+                        description = "shadow config";
+                    };
+                };
+            };
+            default = {};
+            description = "layout config";
+        };
+        spawn-at-startup = mkOption {
+            type = types.listOf types.str;
+            default = [];
+        };
+        hotkey-overlay = mkOption {
+            type = types.submodule {
+                options = {
+                    skip-at-startup = mkOption {
+                        type = types.bool;
+                        default = false;
+                    };
+                };
+            };
+            default = {};
+            description = "hotkeys-overlay";
+        };
+        screenshot-path = mkOption {
+            type = types.str;
+            default = "~/Pictures/Screenshots/Screenshot from %Y-%m-%d %H-%M-%S.png";
+        };
+        animations = mkOption {
+            type = types.submodule {
+                options = {
+                    off = mkOption {
+                        type = types.bool;
+                        default = false;
+                    };
+                };
+            };
+            default = {};
+            description = "animations config";
+        };
+        binds = mkOption {
+            type = types.listOf types.str;
+            default = [];
+        };
     };
 
     config = mkIf cfg.enable {
@@ -224,8 +386,57 @@ in
                 ${if cfg.input.trackpoint.middle-emulation then "middle-emulation" else ""}
 
             }
-
         }
+
+        layout {
+            gaps ${toString cfg.layout.gaps}
+            center-focused-column ${toString cfg.layout.center-focused-column}
+
+            preset-column-widths {
+                ${lib.concatMapStrings (x: "proportion ${toString x}\n}") cfg.layout.preset-column-widths.proportions}
+            }
+
+            default-column-width { proportion ${toString cfg.layout.default-column-width.proportion} }
+
+            focus-ring {
+                ${if cfg.layout.focus-ring.off then "off" else "// off"}
+                active-color ${cfg.layout.focus-ring.active-color}
+                inactive-color ${cfg.layout.focus-ring.inactive-color}
+            }
+
+            border {
+                ${if cfg.layout.border.off then "off" else "// off"}
+                width ${toString cfg.layout.border.width}
+                active-color ${cfg.layout.border.active-color}
+                inactive-color ${cfg.layout.border.inactive-color}
+                urgent-color ${cfg.layout.border.urgent-color}
+            }
+
+            shadow {
+                ${if cfg.layout.shadow.on then "on" else "// on"}
+                softness ${toString cfg.layout.shadow.softness}
+                spread ${toString cfg.layout.shadow.spread}
+                offset ${toString cfg.layout.shadow.offset}
+                color ${cfg.layout.shadow.color}
+            }
+        }
+
+        ${builtins.concatStringsSep "" (
+            map (x: "spawn-at-startup \"${x}\"\n") cfg.spawn-at-startup
+        )}
+
+        hotkey-overlay {
+            ${if cfg.hotkey-overlay.skip-at-startup then "skip-at-startup" else "// skip-at-startup"}
+        }
+
+        screenshot-path ${cfg.screenshot-path}
+
+        binds {
+            ${builtins.concatStringsSep "" (
+                map (x: "${x}\n") cfg.binds
+            )}
+        }
+
 
         '';
     };
--- justfile
+++ justfile
@@ -4,7 +4,7 @@ rebuild_nix:
     sudo nixos-rebuild switch --flake .#laptop
 
 rebuild_home:
-    home-manager switch --flake .#thomas
+    home-manager switch --flake .#thomas --show-trace
 
 rebuild_cool:
     home-manager switch --flake .#thomas &> log &
