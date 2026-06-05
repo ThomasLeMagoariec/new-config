d190482ddab25350c312f1a12ccf72805b63a573
thomas <thomas.lemagoariec@outlook.com>
2026-06-05 10:18:34 +0200

    continuation

--- nix-modules/networking.nix
+++ nix-modules/networking.nix
@@ -1,11 +1,5 @@
-{ spec, lib, ...}:
-let
-    validateHostName = host:
-        if (lib.and (builtins.isString host) (host != "no host name set"))
-            then host
-        else
-            builtins.warn "'${toString host}' is invalid defaulting to 'nixos'" "nixos";
-in {
-    networking.hostName = validateHostName (spec.hostName or "no host name set");
+{ lib, ...}:
+{
+    networking.hostName = "nixos";
     networking.networkmanager.enable = true;
 }
